b88c8250fe341f41f9d569115eadc2298d5efb3f
Sahachat Hukhiao <autogran41@gmail.com>
2023-04-22 18:21:42 +0700

    Format app doc

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const PounchDB = require('pouchdb')
 const url = ""
 
 const store = new Store();
-const db = new PounchDB(path.join(__dirname.replace('\\resources\\app.asar',''),'eventDB'))
+const db = new PounchDB(path.join(__dirname.replace('\\resources\\app.asar', ''), 'eventDB'))
 const createWindow = async () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
@@ -20,43 +20,41 @@ const createWindow = async () => {
   })
   const childWin = new BrowserWindow({
     parent: mainWindow,
-    frame:false,
-    opacity:0.9,
-    transparent:true,
+    frame: false,
+    opacity: 0.9,
+    transparent: true,
     width: 450,
     height: 200,
-    show:false,
+    show: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
-
- 
   mainWindow.removeMenu()
   mainWindow.setMenu(null)
   await mainWindow.loadFile('src/index.html')
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
-  mainWindow.on('minimize',async function(event){
+  mainWindow.on('minimize', async function (event) {
     event.preventDefault();
     if (process.platform === 'darwin') {
       app.dock.hide();
     }
     mainWindow.hide();
-    if(store.has("child-pos")&&store.has("child-size")){
+    if (store.has("child-pos") && store.has("child-size")) {
       const pos = store.get("child-pos")
       const size = store.get("child-size")
-      childWin.setPosition(pos[0],pos[1])
+      childWin.setPosition(pos[0], pos[1])
     }
     childWin.removeMenu()
     childWin.setMenu(null)
     childWin.loadFile('src/index_child.html')
-    childWin.webContents.openDevTools()
+    // childWin.webContents.openDevTools()
     childWin.show();
   });
 
-  mainWindow.on ('close', () => { 
+  mainWindow.on('close', () => {
     store.set("child-pos", childWin.getPosition())
     store.set("child-size", childWin.getSize())
   });
@@ -64,16 +62,20 @@ const createWindow = async () => {
   var appIcon = null;
   appIcon = new Tray(path.join(__dirname, 'app-icon.png'));
   var contextMenu = Menu.buildFromTemplate([
-    { label: 'Show App', click:  function(){
+    {
+      label: 'Show App', click: function () {
         mainWindow.show();
         store.set("child-pos", childWin.getPosition())
         store.set("child-size", childWin.getSize())
         childWin.hide();
-    } },
-    { label: 'Quit', click:  function(){
+      }
+    },
+    {
+      label: 'Quit', click: function () {
         app.isQuiting = true;
         app.quit();
-    } }
+      }
+    }
   ]);
   appIcon.setToolTip('Counties');
   appIcon.setContextMenu(contextMenu);
@@ -87,7 +89,7 @@ app.whenReady().then(() => {
     path: app.getPath('exe'),
   });
 
-  if(!store.has("auto-launch")){
+  if (!store.has("auto-launch")) {
     store.set("auto-launch", false)
   }
   store.get('auto-launch') ? autoLaunch.enable() : autoLaunch.disable()
@@ -99,25 +101,25 @@ app.whenReady().then(() => {
   })
 })
 
-ipcMain.handle("close-win", ()=>{
+ipcMain.handle("close-win", () => {
   app.quit()
 })
 
-ipcMain.handle("add-task", async (event, data) =>{
-  try{
+ipcMain.handle("add-task", async (event, data) => {
+  try {
     const res = await db.post(data)
     return res
-  }catch(err){
+  } catch (err) {
     console.log(err)
   }
-} )
+})
 
-ipcMain.handle("get-tasks", async () =>{
-    var res = await db.allDocs({
-      include_docs: true
-    });
-    return res.rows
-} )
+ipcMain.handle("get-tasks", async () => {
+  var res = await db.allDocs({
+    include_docs: true
+  });
+  return res.rows
+})
 
 ipcMain.handle("done-task", async (event, data) => {
   const doc_id = data;
@@ -150,8 +152,8 @@ ipcMain.handle("delete-task", async (event, data) => {
   return res
 })
 
-ipcMain.handle('delete-batch-tasks', async (event, data)=>{
-  var docs = data.map((t) => {return{_id: t._id, _rev: t._rev, _deleted: true}})
+ipcMain.handle('delete-batch-tasks', async (event, data) => {
+  var docs = data.map((t) => { return { _id: t._id, _rev: t._rev, _deleted: true } })
   console.log(docs)
   try {
     var res = await db.bulkDocs(docs);
@@ -193,4 +195,4 @@ app.on('window-all-closed', () => {
 
 try {
   require('electron-reloader')(module)
-} catch (_) {}
+} catch (_) { }
--- preload.js
+++ preload.js
@@ -8,20 +8,20 @@ contextBridge.exposeInMainWorld('electronAPI', {
   deleteBatchTasks: (payload) => ipcRenderer.invoke('delete-batch-tasks', payload),
   getTasks: () => ipcRenderer.invoke('get-tasks'),
   updateDateTask: (payload) => ipcRenderer.invoke('update-date-task', payload),
-  closeWindow: ()=> ipcRenderer.invoke('close-win'),
-  setAutoLaunch: (payload)=> ipcRenderer.invoke('set-auto-launch', payload)
+  closeWindow: () => ipcRenderer.invoke('close-win'),
+  setAutoLaunch: (payload) => ipcRenderer.invoke('set-auto-launch', payload)
 })
 
 // All the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-      const element = document.getElementById(selector)
-      if (element) element.innerText = text
-    }
-  
-    for (const dependency of ['chrome', 'node', 'electron']) {
-      replaceText(`${dependency}-version`, process.versions[dependency])
-    }
-  })
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+})
 
